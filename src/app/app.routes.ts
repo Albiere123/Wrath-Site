3de5f4425d9061eeb10378015e43685e69496fe6
Albiere123 <albieregames30@gmail.com>
2025-08-14 21:17:42 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {Pag2} from './pag2/pag2'
+import { Inicio } from './inicio/inicio';
+export const routes: Routes = [
+    {path: '', redirectTo: '/inicio', pathMatch: 'full'},
+    {path: 'suporte', component: Pag2},
+    {path: 'inicio', component: Inicio},
+];
